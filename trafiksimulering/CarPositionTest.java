238672be807c9c1ff3d25f731136fa45fedf759f
Emfro <Emfro002@gmail.com>
2015-01-09 12:56:47 +0100

    Wired

---
+++ trafiksimulering/CarPositionTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author emil
+ *
+ */
+public class CarPositionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
